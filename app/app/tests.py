6390e1853ca86b11048291708028a9ac2e525d16
Josh Retallick <josh@old-empire.co.uk>
2024-01-22 17:12:48 +0000

    Lint

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,11 +4,13 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
-        res = calc.add(5,6)
+
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
+
         res = calc.subtract(10, 15)
 
         self.assertAlmostEqual(res, 5)
