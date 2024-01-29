24352bd1a46abac6c58085ef3e7c587169d8ad69
Josh Retallick <josh@old-empire.co.uk>
2024-01-29 16:07:40 +0000

    Ignore files

--- app/app/calc.py
+++
@@ -1,9 +0,0 @@
-"""Calc"""
-
-
-def add(x, y):
-    return x + y
-
-
-def subtract(x, y):
-    return y - x
--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
+from django.shortcuts import render # nosec
 
 # Create your views here.
