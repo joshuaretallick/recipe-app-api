9516563d19e2490726dae1238651007425b1b829
Josh Retallick <josh@old-empire.co.uk>
2024-01-30 13:56:17 +0000

    Linted whitespace

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -22,7 +22,7 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
-    
+
     def update(self, instance, validated_data):
         """Update and return user"""
         password = validated_data.pop('password', None)
@@ -31,7 +31,7 @@ class UserSerializer(serializers.ModelSerializer):
         if password:
             user.set_password(password)
             user.save()
-        
+
         return user
 
 
