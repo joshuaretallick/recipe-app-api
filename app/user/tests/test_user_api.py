e87c51291064b3c55ea93a9fc6fd95afaf83260c
Josh Retallick <josh@old-empire.co.uk>
2024-01-30 09:40:26 +0000

    Linted code

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -5,8 +5,8 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-from rest_framework.test import APIClient 
-from rest_framework import status 
+from rest_framework.test import APIClient
+from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 
@@ -29,7 +29,7 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name',
         }
-        res =self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
